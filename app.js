5cd1abb0e5dbb1ad2325907697143b39d261c9c9
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-13 11:41:25 +0300

    create and mount 'about'

--- app.js
+++ app.js
@@ -15,3 +15,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.render('index', {title: 'Home', message: "Hello There"})
 })
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
