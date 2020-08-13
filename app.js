2746550adb28b74be1d72b15a2f8e3760c57b508
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-13 10:12:28 +0300

    setup pug template engine

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ const app = express();
 
 app.listen(3000, () => console.log("Listening on 3000"))
 
-app.get('/', (req, res) => res.send('Hello'))
+// setup template engine
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// create the home url
+app.get('/', (req, res) => {
+    res.render('index', {title: 'Home', message: "Hello There"})
+})
