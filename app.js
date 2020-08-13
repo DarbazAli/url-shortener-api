82c6f836402c7f227a312a9c54e4fd0e6b7fd0e4
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-13 10:46:19 +0300

    add styles

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.listen(3000, () => console.log("Listening on 3000"))
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+// serve static files
+app.use(express.static(__dirname + '/public'));
+
 // create the home url
 app.get('/', (req, res) => {
     res.render('index', {title: 'Home', message: "Hello There"})
